f8e48ca209a067b6bd469aa90beedfbf974715fa
Cyril Marceau <cyril.marceau.dev@gmail.com>
2024-09-30 23:35:55 +0200

    refactor(todo): use ObservableObject instead local array for save list of todo

--- TodoList/Features/TodoList/Views/TodoListView.swift
+++ TodoList/Features/TodoList/Views/TodoListView.swift
@@ -10,148 +10,7 @@ import OSLog
 
 struct TodoListView: View {
     
-    let todos = [
-        Todo(
-            title: "Faire les courses",
-            description: "Acheter des fruits, légumes et du pain",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .medium,
-            dueDate: Calendar.current.date(byAdding: .day, value: 1, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Réviser pour l'examen",
-            description: "Revoir les chapitres 3 à 5 du cours de mathématiques",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .high,
-            dueDate: Calendar.current.date(byAdding: .day, value: 5, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Appeler le plombier",
-            description: "Prendre rendez-vous pour réparer le robinet qui fuit",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .low,
-            dueDate: Calendar.current.date(byAdding: .day, value: 3, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Faire du sport",
-            description: "30 minutes de jogging dans le parc",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .medium,
-            dueDate: Calendar.current.date(byAdding: .day, value: 1, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Payer les factures",
-            description: "Régler les factures d'électricité et d'internet",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .high,
-            dueDate: Calendar.current.date(byAdding: .day, value: 2, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Nettoyer la maison",
-            description: "Passer l'aspirateur et faire la poussière",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .low,
-            dueDate: nil,
-            isFavorite: false
-        ),
-        Todo(
-            title: "Préparer la présentation",
-            description: "Finaliser les diapositives pour la réunion de vendredi",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .high,
-            dueDate: Calendar.current.date(byAdding: .day, value: 4, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Faire les courses",
-            description: "Acheter des fruits, légumes et du pain",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .medium,
-            dueDate: Calendar.current.date(byAdding: .day, value: 1, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Réviser pour l'examen",
-            description: "Revoir les chapitres 3 à 5 du cours de mathématiques",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .high,
-            dueDate: Calendar.current.date(byAdding: .day, value: 5, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Appeler le plombier",
-            description: "Prendre rendez-vous pour réparer le robinet qui fuit",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .low,
-            dueDate: Calendar.current.date(byAdding: .day, value: 3, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Faire du sport",
-            description: "30 minutes de jogging dans le parc",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .medium,
-            dueDate: Calendar.current.date(byAdding: .day, value: 1, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Payer les factures",
-            description: "Régler les factures d'électricité et d'internet",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .high,
-            dueDate: Calendar.current.date(byAdding: .day, value: 2, to: Date()),
-            isFavorite: false
-        ),
-        Todo(
-            title: "Nettoyer la maison",
-            description: "Passer l'aspirateur et faire la poussière",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .low,
-            dueDate: nil,
-            isFavorite: false
-        ),
-        Todo(
-            title: "Préparer la présentation",
-            description: "Finaliser les diapositives pour la réunion de vendredi",
-            isCompleted: false,
-            createdAt: Date(),
-            updatedAt: Date(),
-            priority: .high,
-            dueDate: Calendar.current.date(byAdding: .day, value: 4, to: Date()),
-            isFavorite: false
-        )
-    ]
+    @StateObject private var todos = TodoListViewModel()
     
     init() {
         let appearance = UINavigationBarAppearance()
@@ -176,28 +35,27 @@ struct TodoListView: View {
     
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(todos) { todo in
-                    NavigationLink(
-                        destination: TodoDetailView(todo: todo)
-                    ) {
-                        VStack(alignment: .leading){
-                            Text(todo.title).font(.headline)
-                            Text(todo.description).font(.subheadline)
-                            
-                        }.contextMenu{
-                            Button(action: addToFavorite) {
-                                Label("Add to Favorites", systemImage: "heart")
-                            }
-                            Button(role: .destructive, action: remove) {
-                                Label("Delete", systemImage: "trash")
-                            }
+            List(todos.todoList) { todo in
+                NavigationLink(
+                    destination: TodoDetailView(todo: todo)
+                ) {
+                    VStack(alignment: .leading){
+                        Text(todo.title).font(.headline)
+                        Text(todo.description).font(.subheadline)
+                        
+                    }.contextMenu{
+                        Button(action: addToFavorite) {
+                            Label("Add to Favorites", systemImage: "heart")
+                        }
+                        Button(role: .destructive, action: remove) {
+                            Label("Delete", systemImage: "trash")
                         }
                     }
                 }
+                
             }
         }
-        .listStyle(.plain)
+        .listStyle(.insetGrouped)
         .navigationTitle("To-Do List")
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
