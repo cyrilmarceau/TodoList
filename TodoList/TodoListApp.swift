986a75ffb355eb8cda8099f94c467d7c4fb2ec44
Cyril Marceau <cyril.marceau.dev@gmail.com>
2024-09-30 20:01:03 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Cyril Marceau on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoListTests/TodoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListTests.swift
+//  TodoListTests
+//
+//  Created by Cyril Marceau on 30/09/2024.
+//
+
+import Testing
+@testable import TodoList
+
+struct TodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
